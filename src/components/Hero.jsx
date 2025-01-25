fa5d91765c20359ab221ccdab2abe635b87736e8
Hemanth-4301 <hemanthachar526@gmail.com>
2025-01-25 12:41:56 +0530

    first changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
           className="md:w-1/2 text-center md:text-left"
         >
           <motion.h1
-            className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold mb-6 leading-tight"
+            className="text-5xl sm:text-5xl md:text-5xl lg:text-7xl font-extrabold mb-6 leading-tight"
             initial={{ opacity: 0, y: -30 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
